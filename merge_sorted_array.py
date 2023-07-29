a5c29c9162d050d1ce5c1164f6e18e998e7a59f3
Phineas Pham <72993615+Ph1n-Pham@users.noreply.github.com>
2023-07-29 14:55:10 -0400

    Create merge_sorted_array.py

---
+++ merge_sorted_array.py
@@ -0,0 +1,59 @@
+"""
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        ### NOTE : we define 2 pointers (p, q) here
+        p, q = m-1, n-1
+        ### NOTE : the while loop conditions
+        while p >= 0 and q >= 0:
+            if nums1[p] > nums2[q]:
+                #***** NOTE : WE START FROM p+q+1 index, since that's the count of non-zero elements in nums1, and nums2
+                nums1[p+q+1] = nums1[p]
+                p = p-1
+            else:
+                ### NOTE WE START FROM p+q+1 index, reason same as above
+                nums1[p+q+1] = nums2[q]
+                q = q-1
+        # if there're still elements in nums2, we just replace the ones in nums1[:q+1] with them (nums2[:q+1])
+        nums1[:q+1] = nums2[:q+1]
