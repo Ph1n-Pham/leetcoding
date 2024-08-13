7fad0b75e37e5de03c8ced0022d86d918090bf4f
Phineas Pham <72993615+Ph1n-Pham@users.noreply.github.com>
2024-08-13 12:09:37 -0400

    Create invertBinaryTree.py

---
+++ invertBinaryTree.py
@@ -0,0 +1,24 @@
+'''
+https://leetcode.com/problems/invert-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not root:
+            return root
+
+        # Swap the left and right children
+        root.left, root.right = root.right, root.left
+        
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
